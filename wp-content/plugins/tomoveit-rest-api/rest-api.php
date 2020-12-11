5d5974d3854017a03114e95fc8ae90e8af7beec4
Tom Hansson <tom.hansson@ecsolutions.se>
2020-12-11 17:23:37 +0100

    Commented out dynomodb stuff for now

--- wp-content/plugins/tomoveit-rest-api/rest-api.php
+++ wp-content/plugins/tomoveit-rest-api/rest-api.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
     die( 'Silence is golden.' );
 }
 
-require 'vendor/autoload.php';
+/*require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 use Aws\DynamoDb\DynamoDbClient;
-use Aws\Credentials\CredentialProvider;
+use Aws\Credentials\CredentialProvider;*/
 
 class TomoveitRestApi_Routes {
 
@@ -16,12 +16,12 @@ class TomoveitRestApi_Routes {
         $version = '1';
         $namespace = 'TomoveitRestApi/v' . $version;
 
-        register_rest_route($namespace, '/data', [
+        /*register_rest_route($namespace, '/data', [
             [
                 'methods' => WP_REST_Server::READABLE,
                 'callback' => [$this, 'rest_get_data'],
             ],
-        ]);
+        ]);*/
 
         register_rest_route($namespace, '/login', [
             [
@@ -40,7 +40,7 @@ class TomoveitRestApi_Routes {
         ]);
     }
 
-    public function rest_get_data() {
+    /*public function rest_get_data() {
         $provider = CredentialProvider::defaultProvider();
 
         $client = new DynamoDbClient([
@@ -52,7 +52,7 @@ class TomoveitRestApi_Routes {
             'TableName' => 'ToMoveItBandData'
         ));
         return $result['Table'];
-    }
+    }*/
 
     public function rest_login($request) {
         $pin = $request->get_param('pin');
