6b5ede77d44bd64d8cbb379373ae0fa465223991
Tom Hansson <tom.hansson@ecsolutions.se>
2020-12-14 20:41:53 +0100

    Rest api buggfix

--- wp-content/plugins/tomoveit-rest-api/rest-api.php
+++ wp-content/plugins/tomoveit-rest-api/rest-api.php
@@ -138,7 +138,7 @@ class TomoveitRestApi_Routes {
 
         $get_selected_post_data = $this->prepare_post_data($post);
 
-        return (object) $get_selected_post_data;
+        return $get_selected_post_data;
     }
 
     public function prepare_post_data($post_id) {
