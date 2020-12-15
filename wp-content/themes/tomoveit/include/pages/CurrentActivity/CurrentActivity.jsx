fe3062a623fcff1df25c792278a93284dc1f4d89
Tom Hansson <tom.hansson@ecsolutions.se>
2020-12-15 08:58:17 +0100

    Added content to current activity

--- wp-content/themes/tomoveit/include/pages/CurrentActivity/CurrentActivity.jsx
+++ wp-content/themes/tomoveit/include/pages/CurrentActivity/CurrentActivity.jsx
@@ -6,18 +6,44 @@ import { useSelector } from 'react-redux';
 const style = classNames.bind(styles);
 
 const CurrentActivity = () => {
-  const runningActivity = useSelector(state => state.app.runningActivity);
+  const runningActivity = useSelector(state => state.app.runningActivity[0]);
+  const titleColor = runningActivity.group ? 'card-current__text--blue' : 'card-current__text--green';
   return (
     <div className={ style('current-activity')} >
-      <div className={ style('single-activity')}>
-        <h1>{runningActivity.title}</h1>
-        <p className={ style('single-activity__text-title')}>Beskrivning</p>
+      <div className={ style('current-activity__title')}>
+        <h1>Du har valt en aktivitet</h1>
+        <p>Försök göra aktiviteten under dagen. Glöm inte att ha skoj 😄</p>
+      </div>
+      <div className={ style('current-activity__warp')}>
+        <div key={runningActivity.title} className={style('card-current')}>
+          <div className={style('card-current_image-container')}>
+            { !runningActivity.group &&
+            <svg className={style('card-current__svg')}>
+              <use xlinkHref={'wp-content/themes/tomoveit/dist/spritemap.svg#order-icon-single'}/>
+            </svg>
+            }
+            { runningActivity.group &&
+            <svg className={style('card-current__svg')}>
+              <use xlinkHref={'wp-content/themes/tomoveit/dist/spritemap.svg#order-icon-group'}/>
+            </svg>
+            }
+            <img className={style('card-current__image')} src={runningActivity.image} alt={'Alt'} />
+          </div>
+          <div className={style('card-current__text')}>
+            <p className={style(titleColor)}>{runningActivity.title}</p>
+            <p>{runningActivity.time}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={ style('current-activity__description')}>
+        <p className={ style('current-activity__text-title')}>Beskrivning</p>
         <p>{runningActivity.description}</p>
-        <p className={ style('single-activity__text-title')}>Vad du behöver</p>
+        <p className={ style('current-activity__text-title')}>Vad du behöver</p>
         <p>{runningActivity.needed}</p>
-        <p className={ style('single-activity__text-title')}>Antal</p>
+        <p className={ style('current-activity__text-title')}>Antal</p>
         <p>{runningActivity.numbers}</p>
-        <p className={ style('single-activity__text-title')}>Instruktioner</p>
+        <p className={ style('current-activity__text-title')}>Instruktioner</p>
         <p>{runningActivity.instruction}</p>
       </div>
     </div>
--- wp-content/themes/tomoveit/include/pages/Introduction/Introduction.jsx
+++ wp-content/themes/tomoveit/include/pages/Introduction/Introduction.jsx
@@ -3,14 +3,27 @@ import BottomContainer from '../../components/Presentational/BottomContainer/Bot
 import Button from '../../components/Button/Button.jsx';
 import Container from '../../components/Container/Container.jsx';
 
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 const Introduction = () => {
+  const history = useHistory();
+  const runningActivity = useSelector(state => state.app.runningActivity[0]);
+  const handleClick = () => {
+    if (runningActivity) {
+      history.push({ pathname: '/runningActivity' });
+    } else {
+      history.push({ pathname: '/activities' });
+    }
+  };
+
   return (
     <Container>
       <p>Jag har följt dig ett tag via ditt armband. Du har rört dig bra. Snyggt jobbat! 💪🏻</p>
       <p>Nu ska jag även följa dig här. Och här kommer du att få välja en ny utmaning att prova varje dag. Kul va? Och jag kommer fråga dig varje dag hur gårdagens utmaning har gått för dig. </p>
       <p>Ska vi köra igång?</p>
       <BottomContainer>
-        <Button to={'/activities'} text={'YES!'}/>
+        <Button to={'/activities'} text={'YES!'} handleClick={handleClick}/>
       </BottomContainer>
     </Container>
   );
