7532a935ce32553a2ff0668ca0588c711487e1c5
Tom Hansson <tom.hansson@ecsolutions.se>
2021-01-07 13:51:20 +0100

    Added inactivity timer

--- wp-content/themes/tomoveit/include/components/PrivateRoute/PrivateRoute.jsx
+++ wp-content/themes/tomoveit/include/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Route, Redirect, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPin } from '../../actions/app';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const pin = useSelector(state => state.app.pin);
+  const [seconds, setSeconds] = useState(600);
+  const [timeout, setTimeoutRedirect] = useState(false);
+
+  useEffect(() => {
+    if (seconds > 0) {
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+      console.log(seconds);
+    } else {
+      setTimeoutRedirect(true);
+      dispatch(setPin(''));
+      history.push('/');
+    }
+  });
 
   return (
     <Route
